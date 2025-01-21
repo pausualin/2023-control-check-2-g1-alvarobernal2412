93ab41ed8c84df4c053aba8c1b8b8f59a7fcd98a
pausualin <pausualin@alum.us.es>
2025-01-21 19:55:14 +0100

    Test 1

--- src/main/java/org/springframework/samples/petclinic/disease/Symptom.java
+++ src/main/java/org/springframework/samples/petclinic/disease/Symptom.java
@@ -2,14 +2,25 @@ package org.springframework.samples.petclinic.disease;
 
 import java.util.Set;
 
+import org.springframework.samples.petclinic.model.NamedEntity;
+
+import jakarta.persistence.Entity;
 import jakarta.persistence.Transient;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Symptom {
+@Entity
+public class Symptom extends NamedEntity{
+
     String description;
+
+    @NotNull
+    @Min(1)
+    Integer dose; 
     
     @Transient
     Set<Disease> includes;
--- src/main/java/org/springframework/samples/petclinic/disease/Treatment.java
+++ src/main/java/org/springframework/samples/petclinic/disease/Treatment.java
@@ -2,13 +2,17 @@ package org.springframework.samples.petclinic.disease;
 
 import java.util.Set;
 
+import org.springframework.samples.petclinic.model.NamedEntity;
+
+import jakarta.persistence.Entity;
 import jakarta.persistence.Transient;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Treatment {
+@Entity
+public class Treatment extends NamedEntity{
     String description;
     Integer dose;
     @Transient
